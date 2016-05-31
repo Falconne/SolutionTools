de773c43123e56ad28cdce23eba15872301866f2
Anuradha Dissanayake <Anuradha.Dissanayake@enghouse.com>
2016-05-31 13:31:52 +1200

    Initial commit

---
+++ SolutionTools/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void AddProjectToSolution(string sourceSolutionName, 
+            string sourceProjectName, string targetSolutionName)
+        {
+            var sourceSolution = Onion.SolutionParser.Parser.SolutionParser.Parse(sourceSolutionName);
+            var sourceProject = sourceSolution.Projects.Where(p => p.Name.Equals(sourceSolutionName));
+        }
+    }
+}
